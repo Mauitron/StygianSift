883fdd4281cff317321433a8c97bd42bcd42e7d0
Charon <Maui_The_Magnificent@proton.me>
2024-11-27 18:41:22 +0100

    Update mouse.rs

--- src/the_tome/mouse.rs
+++ src/the_tome/mouse.rs
@@ -1,3 +1,24 @@
+/*
+* Stygian Sift - A Terminal-based File Manager
+ * Copyright (C) 2024 Maui The Magnificent (Charon)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Contact: Maui_The_Magnificent@proton.me
+ * Project repository: https://github.com/Mauitron/StygianSift.git
+ */
+
 use super::*;
 pub struct ConfirmationWindow {
     pub message: String,
